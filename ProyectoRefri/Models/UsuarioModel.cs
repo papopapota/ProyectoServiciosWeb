4bda68de8f03f4bb88d9de6b119eead5fe163a0d
Carlos Rosales <Carlosrosales21092002@hotmail.com>
2023-11-04 09:57:24 -0500

    View(IniciarSesion, Registrar)

---
+++ ProyectoRefri/Models/UsuarioModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoRefri.Models
+{
+    public class UsuarioModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public int idUsuario { get; set; }
+
+        [Required]
+        [Display(Name = "Nombres")]
+        public string nombres { get; set; }
+
+        [Required]
+        [Display(Name = "Apellidos")]
+        public string apellidos { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Required]
+        [Display(Name = "Telefono")]
+        public string telefono { get; set; }
+
+        [Required]
+        [Display(Name = "password")]
+        public string contrasena { get; set; }
+
+        [Required]
+        [Display(Name = "tipo Usuario")]
+        public int tipoUsuario { get; set; }
+
+        [Required]
+        [Display(Name = "estado")]
+        public int estado { get; set; }
+
+        [Required]
+        [Display(Name = "idMembrecia")]
+        public int idMembrecia { get; set; }
+
+
+
+    }
+}
