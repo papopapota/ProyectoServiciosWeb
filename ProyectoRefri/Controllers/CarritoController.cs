20ee1ad31e68779f5e7ed3b5e22f3e11fa28b0b2
Funky135 <SlayerSlug2015@gmail.com>
2023-11-08 02:58:21 -0500

    DetalleReceta Terminado, Carrito Terminado

---
+++ ProyectoRefri/Controllers/CarritoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoRefri.Controllers
+{
+    public class CarritoController : Controller
+    {
+        private readonly IConfiguration _confi;
+
+        public CarritoController(IConfiguration confi)
+        {
+            _confi = confi;
+        }
+
+        public ActionResult ListarCarrito() 
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoRefri/Models/CarritoModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoRefri.Models
+{
+    public class CarritoModel
+    {
+        [Required]public int IdDetalleCarrito { get; set; }
+        [Required]public int idCarrito { get; set; }
+        [Required]public ProductoModel? idProducto { get; set; }
+        [Required]public int cantidad { get; set; }
+        [Required]public decimal precioTotal { get; set; }
+    }
+}
